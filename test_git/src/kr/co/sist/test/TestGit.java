3db9f311410fe08d7aa679384679fb72b2abaa92
owner <owner@owner-PC>
2017-12-12 12:42:32 +0900

    뀨

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		int i=10;
-		System.out.println("Git Hub 연습");
+		System.out.println("Git Hub 연습"+i);
+		System.out.println("수정수정수정");
 	}
-
 }
