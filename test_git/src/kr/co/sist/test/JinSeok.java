0e6cbd59f924e2901c55539f39f7cfae342dfeae
user <user@user-PC>
2017-12-13 14:28:46 +0900

    2차수정

--- test_git/src/kr/co/sist/test/JinSeok.java
+++ test_git/src/kr/co/sist/test/JinSeok.java
@@ -4,6 +4,8 @@ public class JinSeok {
 
 	public static void main(String[] args) {
 		System.out.println("JinSeok");
-	}
+		System.out.println("2:26 ¿€º∫");
+		System.out.println("Ddd");
+	}//mian
 
-}
+}//JinSeok
