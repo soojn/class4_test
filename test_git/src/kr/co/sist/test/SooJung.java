8d9021b860290b29cbea84c782f6ae9241da6854
user <user@user-PC>
2017-12-13 14:34:12 +0900

    김홍기에게선빵맞음

--- test_git/src/kr/co/sist/test/SooJung.java
+++ test_git/src/kr/co/sist/test/SooJung.java
@@ -5,6 +5,7 @@ public class SooJung {
 	public static void main(String[] args) {
 		System.out.println("soojung ");
 		System.out.println("오늘 너무 추워");
+		System.out.println("jinseok 2:33 작성 ");
 	}
 
 }
