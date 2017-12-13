80a898f5e122556c9ff0f9490ad0d2b00b21ce5d
owner <owner@owner-PC>
2017-12-13 14:53:17 +0900

    그지같애

--- test_git/src/kr/co/sist/test/SooJung.java
+++ test_git/src/kr/co/sist/test/SooJung.java
@@ -1,5 +1,7 @@
 package kr.co.sist.test;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class SooJung {
 
 	public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class SooJung {
 		System.out.println("오늘 너무 추워");
 		System.out.println("jinseok 2:33 작성 ");
 		System.out.println("욕나와");
+		System.out.println("꾸에");
 	}
 
 }
